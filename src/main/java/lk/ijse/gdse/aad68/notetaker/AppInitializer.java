d6b23ea589b5f5ab5130fa6b8b2947f80d85dc95
ThanuraSilva <thanurasilva1989@gmail.com>
2024-08-18 12:57:24 +0530

    Project init with add Spring Web and create package structure

---
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/AppInitializer.java
@@ -0,0 +1,22 @@
+package lk.ijse.gdse.aad68.notetaker;
+
+import lk.ijse.gdse.aad68.notetaker.config.WebAppConfig;
+import lk.ijse.gdse.aad68.notetaker.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/controller/NoteController.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse.aad68.notetaker.controller;
+
+public class NoteController {
+}
