aa8f81a664dd7e28e0659dde6071a819a0549e10
ThanuraSilva <thanurasilva1989@gmail.com>
2024-08-18 15:31:42 +0530

    Get and GetAll Notes endpoint created - Init level

--- src/main/java/lk/ijse/gdse/aad68/notetaker/controller/NoteController.java
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/controller/NoteController.java
@@ -1,24 +1,33 @@
 package lk.ijse.gdse.aad68.notetaker.controller;
 
-import lk.ijse.gdse.aad68.notetaker.dto.Note;
+import lk.ijse.gdse.aad68.notetaker.dto.NoteDTO;
 import lk.ijse.gdse.aad68.notetaker.util.AppUtil;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/notes")
 public class NoteController {
    //Todo: CRUD of the note
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> createNote(@RequestBody Note note) {
+    public ResponseEntity<String> createNote(@RequestBody NoteDTO note) {
         //Todo: Handle with BO
         note.setNoteId(AppUtil.createNoteId());
         System.out.println(note);
         return ResponseEntity.ok("Note created successfully");
     }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<NoteDTO> getAllNotes(){
+        return null;
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public NoteDTO getNote(@PathVariable ("noteId") String noteId)  {
+        System.out.println(noteId);
+        return null;
+    }
+
 
 }
--- src/main/java/lk/ijse/gdse/aad68/notetaker/dto/NoteDTO.java
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/dto/NoteDTO.java
@@ -4,11 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.io.Serializable;
-import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Note implements Serializable {
+public class NoteDTO implements Serializable {
     private String noteId;
     private String noteTitle;
     private String noteDesc;
