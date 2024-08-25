f7e3116f3e064bca2c3e6748d77dabf700a34177
ThanuraSilva <thanurasilva1989@gmail.com>
2024-08-25 15:41:00 +0530

    Add configs for DriverManagerDatasource

--- src/main/java/lk/ijse/gdse/aad68/notetaker/config/WebAppRootConfig.java
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/config/WebAppRootConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -25,10 +26,14 @@ public class WebAppRootConfig {
 
     @Bean
     public DataSource dataSource() {
-
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        return builder.setType(EmbeddedDatabaseType.HSQL).build();
+        var dmds = new DriverManagerDataSource();
+        dmds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dmds.setUrl("jdbc:mysql://localhost:3306/noteTakerAAD68?createDatabaseIfNotExist=true");
+        dmds.setUsername("root");
+        dmds.setPassword("mysql");
+        return dmds;
     }
+
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
