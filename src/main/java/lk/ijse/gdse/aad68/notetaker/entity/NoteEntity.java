801d7bebde3dfd091e28f7ce42757717f3b39dc6
ThanuraSilva <thanurasilva1989@gmail.com>
2024-09-01 14:25:15 +0530

    Create entities with proper relationship

--- src/main/java/lk/ijse/gdse/aad68/notetaker/entity/NoteEntity.java
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/entity/NoteEntity.java
@@ -1,8 +1,6 @@
 package lk.ijse.gdse.aad68.notetaker.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +12,12 @@ import java.io.Serializable;
 @Data
 @Table(name = "notes")
 @Entity
-public class NoteEntity implements Serializable {
+public class NoteEntity implements SuperEntity {
     @Id
     private String noteId;
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private UserEntity user;
     private String noteTitle;
     private String noteDesc;
     private String priorityLevel;
