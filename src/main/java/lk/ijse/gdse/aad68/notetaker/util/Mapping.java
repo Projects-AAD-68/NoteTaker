5b4f8aaff24a87e238612cb4fb4ef934c1dc5b5f
ThanuraSilva <thanurasilva1989@gmail.com>
2024-09-29 12:36:08 +0530

    Tested the CRUD of Note and optimize the mapping

--- src/main/java/lk/ijse/gdse/aad68/notetaker/util/Mapping.java
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/util/Mapping.java
@@ -24,7 +24,7 @@ public class Mapping {
         return modelMapper.map(dto, NoteEntity.class);
     }
     public List<NoteDTO> convertToDTO(List<NoteEntity> notes) {
-        return modelMapper.map(notes, List.class);
+        return modelMapper.map(notes, new TypeToken<List<NoteDTO>>() {}.getType());
     }
     //User matters mapping
     public UserEntity convertToUserEntity(UserDTO userDTO) {
